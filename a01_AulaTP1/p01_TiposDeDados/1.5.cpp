f2637e4bfd6414d8dad33ae75fa16924ae32b975
Fábio Gaspar <fabiodanielreis@hotmail.com>
2017-02-15 22:12:04 +0000

    Logic improvement

--- a01_AulaTP1/p01_TiposDeDados/1.5.cpp
+++ a01_AulaTP1/p01_TiposDeDados/1.5.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 int main()
 {
-	int h1, h2, h, m1, m2, m, s1, s2, s, d;
-
-	int h_sum, m_sum, s_sum;
+	int h, m, s, d, total;
 
 	cout << "Tempo 1 (horas minutos segundos) ? ";
-	cin >> h1 >> m1 >> s1;
+	cin >> h >> d >> s;
+    total = h * 3600 + d * 60 + s;
 
 	cout << "Tempo 2 (horas minutos segundos) ? ";
-	cin >> h2 >> m2 >> s2;
+	cin >> h >> d >> s;
+    total += h * 3600 + d * 60 + s;
 
-	// variaveis auxiliares para organizar o código...
-	h_sum = h1 + h2;
-	m_sum = m1 + m2;
-	s_sum = s1 + s2;
+    // Calcular dias, horas, minutos e segundos
+    d = total / 86400;
+    total %= 86400;
+    
+    h = total / 3600;
+    total %= 3600;
 
-    // Cálculo de segundos, minutos, horas, dia
-	s = s_sum % 60;
-	m = (m_sum + s_sum / 60) % 60;
-	h = (h_sum + m_sum / 60) % 24;
-	d = h_sum / 24;
-
+    m = total / 60;
+    s = total % 60;
     //Output
-	cout << "Soma dos tempos: " << d << " dia, "
-		<< h << " horas, " << m << " minutos e "
-		<< s << " segundos\n";
 
-
-	// TODO: tentar reduzir quantidade de variáveis
+    cout << "Soma dos tempos: " << d << " dia, "
+    << h << " horas, " << m << " minutos e " << s << " segundos" << endl;
+    
+	
 }
